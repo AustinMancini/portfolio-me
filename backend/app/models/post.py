1c0351cd2091dcf8dc0949b71bf512a3f7a3ace9
AustinMancini <mancini787@yahoo.com>
2025-02-24 13:30:34 -0500

    db configured with alembic

---
+++ backend/app/models/post.py
@@ -0,0 +1,14 @@
+import uuid
+from sqlalchemy import Column, String, Text, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID 
+from app.core.config import Base
+from datetime import datetime, timezone
+
+class Post(Base):
+    __tablename__ = 'posts'
+    __table_args__ = {'schema': 'public'}
+    id = Column(UUID(as_uuid=True), primary_key=True, unique=True)
+    title = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    author_id = Column(UUID(as_uuid=True), ForeignKey('public.profiles.user_id'), nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
--- backend/app/models/profile.py
+++ backend/app/models/profile.py
@@ -1,13 +1,13 @@
 import uuid
-from sqlalchemy import Column, String, ForeignKey, Boolean
-from sqlalchemy.dialects.postgresql import UUID  # Use the PostgreSQL UUID type
-from core.config import Base
+from sqlalchemy import Column, String, Boolean
+from sqlalchemy.dialects.postgresql import UUID 
+from app.core.config import Base
 
-# Create the Profile model
 class Profile(Base):
     __tablename__ = 'profiles'
+    __table_args__ = {'schema': 'public'}
     # Use Uuid without ForeignKey; the constraint will be added in Supabase
-    user_id = Column(UUID(as_uuid=True), primary_key=True)
+    user_id = Column(UUID(as_uuid=True), primary_key=True, unique=True)
     username = Column(String, unique=True, index=True)
     full_name = Column(String, nullable=True, index=True)
     is_subscribed = Column(Boolean, default=False)
